4096694e85320db34190b9bb212c19b6890c5eb0
Juan Lulkin <maiz@lulk.in>
2016-06-12 00:18:22 +0300

    update book

---
+++ packages/main/internals/History.js
@@ -0,0 +1,45 @@
+import reduce from 'ramda/src/reduce'
+import delay from './delay'
+import map from 'ramda/src/map'
+
+export default class History {
+  constructor (state, reducer, rerender) {
+    this.reduce = reduce(reducer)
+    this.state = state
+    this.subscriptions = []
+    this.delta = []
+    this.rerender = () => rerender(this.state)
+    this.paused = false
+  }
+
+  subscribe (subscription) {
+    this.subscriptions.push(subscription)
+  }
+
+  concat () {
+    this.state = this.reduce(this.state, this.delta)
+    const dom = this.rerender()
+    this.delta = []
+
+    map((subscription) => subscription(this), this.subscriptions)
+    return dom
+  }
+
+  pause () {
+    this.paused = true
+  }
+
+  togglePause () {
+    this.paused = !this.paused
+  }
+
+  unpause () {
+    this.paused = false
+  }
+
+  push (action) {
+    if (this.paused) return
+    this.delta.push(action)
+    return delay(() => this.concat())
+  }
+}
